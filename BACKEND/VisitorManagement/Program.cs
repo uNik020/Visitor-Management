5d05c77bf3cb068564976cf3130ebab2568efa7d
uNik020 <shekharnikhil020@gmail.com>
2025-05-25 15:29:54 +0530

    registration success

--- BACKEND/VisitorManagement/Program.cs
+++ BACKEND/VisitorManagement/Program.cs
@@ -1,13 +1,27 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
-using VisitorManagement.Models;
+using System.Text;
 using VisitorManagement.Controllers;
 using VisitorManagement.DTO;
-using VisitorManagement.Interfaces;
-using VisitorManagement.Services;
 using VisitorManagement.HelperClasses;
+using VisitorManagement.Interfaces;
+using VisitorManagement.Models;
+using VisitorManagement.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 // Add services to the container.
 
@@ -18,6 +32,32 @@ builder.Services.AddTransient<IVisitService, VisitService>();
 builder.Services.AddTransient<IAuthService, AuthService>();
 builder.Services.AddTransient<IEmailService, EmailService>();
 builder.Services.AddTransient<IDepartmentService, DepartmentService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+//default policy which tells to use jwt as a default 
+//Note : this is required if we are using multiple schemes like cookies, openId, etc.
+//we are just including this for future concerns
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder(
+        JwtBearerDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser()
+            .Build();
+});
+
 
 builder.Services.AddDbContext<VisitorManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -34,6 +74,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//use CORS
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
